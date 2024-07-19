e40530681c22c61fb4a489bafe81d5a5c85f789a
ron <jook323@gmail.com>
2024-07-19 17:07:19 +0300

    e

---
+++ components/Button/Button.jsx
@@ -0,0 +1,13 @@
+var count=0
+
+function ONCLICK(){
+    coutn++;
+
+}
+
+
+const Button = (props) =>{
+    return(
+        <Button {...props} className='hover:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-400 focus:ring-opacity-75'/>
+)
+}
