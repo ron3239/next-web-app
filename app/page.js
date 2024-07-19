acfe1bd9286b7d2d5d560de671b5a5471fac5e5a
ron <jook323@gmail.com>
2024-07-19 18:42:28 +0300

    qwe

--- app/page.js
+++ app/page.js
@@ -9,10 +9,8 @@ const HomePage = () => {
   const {tg,username,close_app}=useHook();
 
   return (
-    <div className='text-sm '>
-      <h1 className='text-white'>
-        {username}
-      </h1>
+    <div className='text-black '>
+      <h1 className='place-content-center'>{username}</h1>
       <button className='text-white' onClick={close_app}>Close</button>
     </div>
   )
