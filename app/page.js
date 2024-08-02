cffb3c9e4afbf458a5888a6c510c9da8574e24ab
ron <jook323@gmail.com>
2024-08-02 20:25:30 +0300

    ][

--- app/page.js
+++ app/page.js
@@ -30,51 +30,51 @@ const HomePage = () => {
     }
   }, []);
 
-  // useEffect(() => {
-  //   console.log('Вызов fetchBdUser');
-  //   const fetchBdUser = async () => {
-  //     try {
-  //       const response = await fetch('/api/user/search', {
-  //         method: "POST",
-  //         headers: {
-  //           'Content-Type': 'application/json',
-  //         },
-  //         body: JSON.stringify({
-  //           id_user: tgData?.user?.id,
-  //         }),
-  //       });
-  //       console.log("send")
-  //       const data = await response.json();
-  //       setBdUser(data);
-  //       console.log(data);
-  //       if (data === null) {
-  //         try {
-  //           const res = await fetch('/api/user/create', {
-  //             method: "POST",
-  //             headers: {
-  //               'Content-Type': 'application/json',
-  //             },
-  //             body: JSON.stringify({
-  //               id_user: tgData?.user?.id,
-  //               username: tgData?.user?.username,
-  //               last_update_time: new Date()
-  //             }),
-  //           })
-  //         } catch (e) {
-  //           console.log(`ERROR CREATE USER ${e}`)
-  //         };
-  //       }
+  useEffect(() => {
+    console.log('Вызов fetchBdUser');
+    const fetchBdUser = async () => {
+      try {
+        const response = await fetch('/api/user/search', {
+          method: "POST",
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            id_user: tgData?.user?.id,
+          }),
+        });
+        console.log("send")
+        const data = await response.json();
+        setBdUser(data);
+        console.log(data);
+        if (data === null) {
+          try {
+            const res = await fetch('/api/user/create', {
+              method: "POST",
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                id_user: tgData?.user?.id,
+                username: tgData?.user?.username,
+                last_update_time: new Date()
+              }),
+            })
+          } catch (e) {
+            console.log(`ERROR CREATE USER ${e}`)
+          };
+        }
 
-  //     } catch (error) {
-  //       setBdUser(null);
-  //       console.error('Ошибка получения данных пользователя:', error);
-  //     }
-  //   };
+      } catch (error) {
+        setBdUser(null);
+        console.error('Ошибка получения данных пользователя:', error);
+      }
+    };
 
-  //   if (tgData) {
-  //     fetchBdUser();
-  //   }
-  // }, [tgData]);
+    if (tgData) {
+      fetchBdUser();
+    }
+  }, [tgData]);
 
   if (!tgData) {
     return <Loading />;
