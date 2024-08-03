afc57e54a2c0cc1413f21715ed04932047019a67
ron <jook323@gmail.com>
2024-08-03 17:38:50 +0300

    ][

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,7 +1,7 @@
-import { NextResponse } from "next/server";
-import { prisma } from "../../../prisma/prisma-cleint";
+// import { NextResponse } from "next/server";
+// import { prisma } from "../../../prisma/prisma-cleint";
 
-export async function GET() {
-    const spisok_upgrade = await prisma.user.findMany();
-    return NextResponse.json(spisok_upgrade);
-};
+// export async function GET() {
+//     const spisok_upgrade = await prisma.user.findMany();
+//     return NextResponse.json(spisok_upgrade);
+// };
