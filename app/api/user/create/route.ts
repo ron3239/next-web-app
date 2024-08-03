60cbb4dfe7dca01418c954f50cc148ed6c7f2eac
ron <jook323@gmail.com>
2024-08-03 16:55:51 +0300

    ][

--- app/api/user/create/route.ts
+++ app/api/user/create/route.ts
@@ -9,13 +9,7 @@ export async function POST(req: NextRequest) {
       data,
     });
 
-    // Преобразуем BigInt в строку перед возвратом
-    const response = {
-      ...user,
-      id_user: user.id_user.toString(),
-    };
-
-    return NextResponse.json(response);
+    return NextResponse.json(user);
   } catch (e) {
     console.error(e);
     return NextResponse.json({ error: `Ошибка ${e.message}` }, { status: 500 });
