7e5d4f026c1554e9644d1d1dca08c5fccd972979
ron <jook323@gmail.com>
2024-08-03 12:16:26 +0300

    /.,

--- app/page.js
+++ app/page.js
@@ -9,76 +9,85 @@ const HomePage = () => {
   const [bdUser, setBdUser] = useState([]);
   const hasFetched = useRef(false);
 
-  useEffect(() => {
-    console.log('Вызов fetchData');
-    const fetchData = async () => {
-      try {
-        const tgData = await initInitData();
-        setTgData(tgData);
-        console.log('====================================')
-        console.log(tgData);
-        console.log('initInitData вызван');
-        console.log('====================================')
-      } catch (error) {
-        setTgData(null);
-        console.error('Ошибка получения данных:', error);
-      }
-    };
-
-    if (!hasFetched.current) {
-      fetchData();
-      hasFetched.current = true;
-    }
-  }, []);
-
+  // useEffect(() => {
+  //   console.log('Вызов fetchData');
+  //   const fetchData = async () => {
+  //     try {
+  //       const tgData = await initInitData();
+  //       setTgData(tgData);
+  //       console.log('====================================')
+  //       console.log(tgData);
+  //       console.log('initInitData вызван');
+  //       console.log('====================================')
+  //     } catch (error) {
+  //       setTgData(null);
+  //       console.error('Ошибка получения данных:', error);
+  //     }
+  //   };
+  
+  //   // Вызываем fetchData только один раз при монтировании
+  //   if (!hasFetched.current) {
+  //     fetchData();
+  //     hasFetched.current = true;
+  //   }
+  // }, []);
+  
   useEffect(() => {
     console.log('Вызов fetchBdUser');
     const fetchBdUser = async () => {
       try {
-        const response = await fetch('/api/user/search', {
-          method: "POST",
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            // id_user: tgData?.user?.id,
-            id_user: 5,
-          }),
-        });
-        console.log("send")
-        const data = await response.json();
-        setBdUser(data);
-        console.log(data);
-        if (data === null) {
-          try {
-            const res = await fetch('/api/user/create', {
+        if (true) {                                                         //!!
+          // Получаем id_user из tgData
+          const idUser = tgData?.user?.id;
 
-              method: "POST",
-              headers: {
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({
-                id_user: tgData?.user?.id,
-                username: tgData?.user?.username,
-                last_update_time: new Date()
-              }),
-            })
-          } catch (e) {
-            console.log(`ERROR CREATE USER ${e}`)
-          };
+
+          const response = await fetch('/api/user/search', {
+            method: "POST",
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              id_user: 5, // Используем id_user из tgData
+            }),
+          });
+          console.log("send")
+          const data = await response.json();
+          setBdUser(data);
+          console.log(data);
+          if (data === null) {
+            try {
+              const res = await fetch('/api/user/create', {
+  
+                method: "POST",
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                  id_user: idUser, // Используем id_user из tgData
+                  username: tgData?.user?.username,
+                  last_update_time: new Date()
+                }),
+              })
+            } catch (e) {
+              console.log(`ERROR CREATE USER ${e}`)
+            };
+          }
+  
         }
-
       } catch (error) {
         setBdUser(null);
         console.error('Ошибка получения данных пользователя:', error);
       }
     };
+  
+    // Вызываем fetchBdUser только при изменении tgData
+    // if (tgData) {
+    //   fetchBdUser();
+    // }
+    fetchBdUser();
+  }, []);
 
-    if (tgData) {
-      fetchBdUser();
-    }
-  }, [tgData]);
-
+  
   if (!tgData) {
     return <Loading />;
   } else {
