efb47560d8bda5b5c20962b839d404c436baa5f0
ron <jook323@gmail.com>
2024-08-03 17:04:09 +0300

    ][

--- app/api/user/create/route.ts
+++ app/api/user/create/route.ts
@@ -1,17 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "../../../../prisma/prisma-cleint";
 
-
-export async function POST(req: NextRequest) {
-  const data = await req.json();
-  try {
-    const user = await prisma.user.create({
-      data,
-    });
-
-    return NextResponse.json(user);
-  } catch (e) {
-    console.error(e);
-    return NextResponse.json({ error: `Ошибка ${e.message}` }, { status: 500 });
-  }
+export async function POST(req:NextRequest) {
+    const data = await req.json()
+    try{
+        const user = await prisma.user.create({
+        data
+        });
+        return NextResponse.json(user);
+}catch(e){
+    return NextResponse.json([{error:`Ошибка ${e}`},],{status:300});
 }
+
+};
--- app/api/user/search/route.ts
+++ app/api/user/search/route.ts
@@ -1,17 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "../../../../prisma/prisma-cleint";
 
-export async function POST(req: NextRequest) {
-  const data = await req.json();
-  try {
+export async function POST(req:NextRequest) {
+    const data = await req.json()
+    try{
     const spisok_user = await prisma.user.findUnique({
-      where: {
-        id_user: data.id_user,
-      },
+        where:{
+            id_user:data.id_user
+        }
     });
-
     return NextResponse.json(spisok_user);
-  } catch (e) {
-    return NextResponse.json([{ error: `Ошибка ${e}` }], { status: 400 });
-  }
+}catch(e){
+    return NextResponse.json([{error:`Ошибка ${e}`}],{status:400});
 }
+
+};
+
