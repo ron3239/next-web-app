0a8eda9b0bf5c8a100f57022df464b5630bed152
ron <jook323@gmail.com>
2024-08-03 13:42:11 +0300

    z

--- app/api/user/search/route.ts
+++ app/api/user/search/route.ts
@@ -1,12 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "../../../../prisma/prisma-cleint";
 
-export async function POST(req:NextRequest) {
+export async function GET(req:NextRequest) {
     const data = await req.json()
     try{
     const spisok_user = await prisma.user.findUnique({
         where:{
-            id_user:5
+            id_user:data.id_user
         }
     });
     return NextResponse.json(spisok_user);
