943c013585489f8d7a0ff19a3127e80f07a6de8f
ron <jook323@gmail.com>
2024-08-03 10:23:15 +0300

    ghj

--- app/page.js
+++ app/page.js
@@ -9,74 +9,74 @@ const HomePage = () => {
   const [bdUser, setBdUser] = useState([]);
   const hasFetched = useRef(false);
 
-  useEffect(() => {
-    console.log('Вызов fetchData');
-    const fetchData = async () => {
-      try {
-        const tgData = await initInitData();
-        setTgData(tgData);
-        console.log('====================================')
-        console.log(tgData);
-        console.log('====================================')
-      } catch (error) {
-        setTgData(null);
-        console.error('Ошибка получения данных:', error);
-      }
-    };
+  // useEffect(() => {
+  //   console.log('Вызов fetchData');
+  //   const fetchData = async () => {
+  //     try {
+  //       const tgData = await initInitData();
+  //       setTgData(tgData);
+  //       console.log('====================================')
+  //       console.log(tgData);
+  //       console.log('====================================')
+  //     } catch (error) {
+  //       setTgData(null);
+  //       console.error('Ошибка получения данных:', error);
+  //     }
+  //   };
 
-    if (!hasFetched.current) {
-      fetchData();
-      hasFetched.current = true;
-    }
-  }, []);
+  //   if (!hasFetched.current) {
+  //     fetchData();
+  //     hasFetched.current = true;
+  //   }
+  // }, []);
 
-  useEffect(() => {
-    console.log('Вызов fetchBdUser');
-    const fetchBdUser = async () => {
-      try {
-        const response = await fetch('/api/user/search', {
-          next: { revalidate: 3600 },
-          method: "POST",
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            id_user: tgData?.user?.id,
-          }),
-        });
-        console.log("send")
-        const data = await response.json();
-        setBdUser(data);
-        console.log(data);
-        if (data === null) {
-          try {
-            const res = await fetch('/api/user/create', {
-              next: { revalidate: 3600 },
-              method: "POST",
-              headers: {
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({
-                id_user: tgData?.user?.id,
-                username: tgData?.user?.username,
-                last_update_time: new Date()
-              }),
-            })
-          } catch (e) {
-            console.log(`ERROR CREATE USER ${e}`)
-          };
-        }
+  // useEffect(() => {
+  //   console.log('Вызов fetchBdUser');
+  //   const fetchBdUser = async () => {
+  //     try {
+  //       const response = await fetch('/api/user/search', {
+  //         next: { revalidate: 3600 },
+  //         method: "POST",
+  //         headers: {
+  //           'Content-Type': 'application/json',
+  //         },
+  //         body: JSON.stringify({
+  //           id_user: tgData?.user?.id,
+  //         }),
+  //       });
+  //       console.log("send")
+  //       const data = await response.json();
+  //       setBdUser(data);
+  //       console.log(data);
+  //       if (data === null) {
+  //         try {
+  //           const res = await fetch('/api/user/create', {
+  //             next: { revalidate: 3600 },
+  //             method: "POST",
+  //             headers: {
+  //               'Content-Type': 'application/json',
+  //             },
+  //             body: JSON.stringify({
+  //               id_user: tgData?.user?.id,
+  //               username: tgData?.user?.username,
+  //               last_update_time: new Date()
+  //             }),
+  //           })
+  //         } catch (e) {
+  //           console.log(`ERROR CREATE USER ${e}`)
+  //         };
+  //       }
 
-      } catch (error) {
-        setBdUser(null);
-        console.error('Ошибка получения данных пользователя:', error);
-      }
-    };
+  //     } catch (error) {
+  //       setBdUser(null);
+  //       console.error('Ошибка получения данных пользователя:', error);
+  //     }
+  //   };
 
-    if (tgData) {
-      fetchBdUser();
-    }
-  }, [tgData]);
+  //   if (tgData) {
+  //     fetchBdUser();
+  //   }
+  // }, [tgData]);
 
   if (!tgData) {
     return <Loading />;
