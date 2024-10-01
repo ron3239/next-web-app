f4472dfc3de0bc77b8d891bc2685ca8f49fd0813
ron <jook323@gmail.com>
2024-10-01 19:32:18 +0300

    5

--- components/Home/Home.jsx
+++ components/Home/Home.jsx
@@ -44,7 +44,7 @@ const Home = (props)=>{
                     <h1 className="text-4xl text-stone-200 font-bold col-span-1 gap-3">
                         {props._metadata.name}
                     </h1>
-                    <h1 className="p-4 text-4xl text-stone-200 font-bold self-center bg-indigo-600 rounded col-span-3">
+                    <h1 className="p-4 text-4xl text-stone-200 font-bold self-center bg-indigo-600 rounded col-span-5">
                         WELCARS
                     </h1>
                 </div>
