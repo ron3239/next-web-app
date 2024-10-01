3c2fb32f68071f25a23e740a4ef73c1655e95636
ron <jook323@gmail.com>
2024-10-01 19:33:51 +0300

    0

--- components/Home/Home.jsx
+++ components/Home/Home.jsx
@@ -41,10 +41,10 @@ const Home = (props)=>{
     return (
         <div className="h-full w-full select-none">
                 <div className="m-2 grid grid-cols-7 ">
-                    <h1 className="text-4xl text-stone-200 font-bold col-span-1 gap-3">
+                    <h1 className="text-4xl text-stone-200 font-bold col-span-1 gap-5">
                         {props._metadata.name}
                     </h1>
-                    <h1 className="p-4 text-4xl text-stone-200 font-bold self-center bg-indigo-600 rounded col-span-5">
+                    <h1 className="p-4 text-4xl text-stone-200 font-bold self-center bg-indigo-600 rounded col-span-4">
                         WELCARS
                     </h1>
                 </div>
