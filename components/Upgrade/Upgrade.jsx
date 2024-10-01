9f8068432242655fc56e18b2da365ba35369b2f8
ron <jook323@gmail.com>
2024-10-01 17:29:41 +0300

    w

--- components/Upgrade/Upgrade.jsx
+++ components/Upgrade/Upgrade.jsx
@@ -173,25 +173,10 @@ const Upgrade = (props) => {
       }
 
     const l = _data.map((list, id) => (
-      <li className='flex flex-col justify-center rounded-lg bg-white p-6 shadow-secondary-1 dark:bg-surface-dark dark:text-white text-surface' key={id}>
-
-        
-          <h5
-            class="mb-2 text-xl font-medium leading-tight">
-            Card title
-          </h5>
-          <p class="mb-4 text-base">
-            With supporting text below as a natural lead-in to additional content.
-          </p>
-          <button
-            type="button"
-            class="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
-            data-twe-ripple-init
-            data-twe-ripple-color="light">
-            onClick={() => { open_close_ramka(); setDataRamka(list); }}
-            Купить
-            </button>
-      </li>
+        <li className='flex flex-col justify-center rounded-lg border border-stone-700 bg-stone-700' key={id}>
+          <h1 className='flex-grow items-center text-slate-100'>{list.name}</h1>
+          <button className='mt-auto place-items-end w-[100%] items-center border-t border-stone-900 text-slate-100' onClick={()=>{open_close_ramka();setDataRamka(list);}}>Купить</button>
+        </li>
       ));
 
     return(
