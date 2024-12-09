a3da12b93aa8374013de23b40940d4acf4b8306b
ron <jook323@gmail.com>
2024-12-09 20:56:29 +0300

    dada

--- components/Register_Login/Register_Login.jsx
+++ components/Register_Login/Register_Login.jsx
@@ -13,7 +13,7 @@ export const Register_Login = () => {
             <div class="mb-5">
                 <label for="Id" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Id</label>
                 <input  id="Id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="123456" required
-                    value={Id} onChange={e=>(setId(toString(e.target.value)))}
+                    value={id} onChange={e=>(setId(toString(e.target.value)))}
                 />
             </div>
             <div class="mb-5">
